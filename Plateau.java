afac91177143a000011501ad3e128c43edbe9bb5
oneXochipilli <64990536+oneXochipilli@users.noreply.github.com>
2022-01-04 22:50:43 +0100

    Start plateau int

---
+++ Plateau.java
@@ -0,0 +1,16 @@
+public class Plateau {
+	private Case [][] g; // g pour grille
+
+	 int [][] plateau = 
+	 {{5 , 1 , 1 , 2 , 1 , 1 , 1 , 5 , 1 , 1 , 1 , 2 , 1 , 1 , 5},
+	  {1 , 4 , 1 , 1 , 1 , 3 , 1 , 1 , 1 , 3 , 1 , 1 , 1 , 4 , 1},
+	  {}
+
+
+	 }
+
+	/*public Plateau () {
+	*	this.g = new
+	}*/
+	
+}
